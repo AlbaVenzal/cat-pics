996163131f3098132d16d4557f2f409277740102
Alba Venzal <alba.venzal@gmail.com>
2022-04-02 12:40:51 +0200

    Init project

---
+++ CatPics/CatPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatPicsApp.swift
+//  CatPics
+//
+//  Created by Alba Venzal on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
